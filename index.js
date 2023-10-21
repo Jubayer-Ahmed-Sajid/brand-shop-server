e5319f6f9e8024e3c6204adcfc132a9f7ffa9013
Jubayer-Ahmed-Sajid <jubayerahmed661ac@gmail.com>
2023-10-21 19:58:32 +0600

    vercel deploy issue resolved

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 const express = require('express')
 const cors = require('cors')
-const port = process.env.PORT || 5000
 require('dotenv').config()
 const app = express()
 app.use(cors())
 app.use(express.json())
-
-
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const port = process.env.PORT || 5000
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vqva6ft.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -22,7 +21,6 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
     const addedProductCollection = client.db('ProductsDB').collection('addedProducts')
     const cartCollection = client.db('ProductsDB').collection('MyCart')
     const productCollection = client.db('ProductsDB').collection('Product')
@@ -111,6 +109,11 @@ async function run() {
       const result = await cursor.toArray()
       res.send(result)
     })
+    app.get('/cart', async (req, res) => {
+      const cursor = cartCollection.find()
+      const result = await cursor.toArray()
+      res.send(result)
+    })
 
     app.get('/cart/:email/:id', async (req, res) => {
       const id = req.params.id
@@ -131,11 +134,9 @@ async function run() {
 
    
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    // await client.close();
   }
 }
 run().catch(console.dir);
